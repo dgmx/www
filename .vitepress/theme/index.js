98042fe59e76ffdbded8526455a95fcbdcc8f56c
Diego J. Gonzalez <diego.gm@gmail.com>
2026-06-18 18:12:30 +0200

    Revertir tema Ghostty, volver al estilo anterior

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -2,7 +2,6 @@ import DefaultTheme from 'vitepress/theme'
 import { onMounted, nextTick, watch, h } from 'vue'
 import { useRoute, useData } from 'vitepress'
 import NotFound from './components/NotFound.vue'
-import './custom.css'
 
 export default {
   extends: DefaultTheme,
